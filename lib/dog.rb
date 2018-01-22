48c774381fcd20f0880d645a84050e7f762ec1c0
Justin K <justin-kim1@hotmail.com>
2018-01-22 04:45:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,27 @@
-class Dog
-   def name=(name)
-    @name = name
-  end
+# class Dog
+#   def name=(name)
+#     @name = name
+#   end
   
-  def name(name)
-    @name = name 
-  end
+#   def name(name)
+#     @name = name 
+#   end
  
+#   def bark
+#     puts "woof!"
+#   end
+# end
+
+class Dog
+  def name
+    @name
+  end
+
+  def name=(dog)
+    @name = dog
+  end
+
   def bark
     puts "woof!"
   end
 end
-
